50358dc739527527dbfd97b80482f338bb3a8979
GerryKp <gerardferre.alu@iespacomolla.es>
2022-02-14 20:58:18 +0100

    Added method to insert data to the database when executing the app for the first time

--- app/src/main/java/com/gerard/proyectof/MainViewModel.kt
+++ app/src/main/java/com/gerard/proyectof/MainViewModel.kt
@@ -12,6 +12,7 @@ import com.gerard.proyectof.entities.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 import java.io.ByteArrayOutputStream
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
@@ -26,6 +27,8 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private var _productos :LiveData<List<Productos>> = prodDao.getAll()
     val productos = _productos
 
+    var inserted = false
+
     fun insertOne(r:Restaurante)
     {
         restdao.insertOne(r)
@@ -38,21 +41,26 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     fun insertData()
     {
-        viewModelScope.launch (Dispatchers.IO){
+        if(!inserted)
+        {
+            viewModelScope.launch (Dispatchers.IO){
 
-            val prods = listOf(Productos(1,"Cafe Americano",1.5f,"Café de máquina solo",1),
-                Productos(2,"Tostada Aceite",2f,"Tostada con aceite y sal",1),
-                Productos(3,"Tostada Tomate",2.5f,"Tostada con tomate",1))
-            val restaurantes = listOf(
-                Restaurante(2, "Restaurante 2",prods,2,"res2","Avenida Constitución 2", null),
-                Restaurante(3, "Restaurante 3",prods,3,"res3","Avenida Constitución 3", null),
-                Restaurante(4, "Restaurante 4",prods,4,"res4","Avenida Constitución 4", null),
-                Restaurante(5, "Restaurante 5",prods,3,"res5","Avenida Constitución 5", null),
-                Restaurante(6, "Restaurante 6",prods,5,"res6","Avenida Constitución 6", null),
-                Restaurante(7, "Restaurante 7",prods,3,"res7","Avenida Constitución 7", null),
-            )
-            restdao.insertAll(restaurantes)
-            //prodDao.insertAll(prods)
+                val prods = listOf(Productos(1,"Cafe Americano",1.5f,"Café de máquina solo",1),
+                    Productos(2,"Tostada Aceite",2f,"Tostada con aceite y sal",1),
+                    Productos(3,"Tostada Tomate",2.5f,"Tostada con tomate",1))
+                val restaurantes: List<Restaurante> = listOf(
+                    Restaurante(1, "Restaurante 1",prods,2,"res1","Avenida Constitución 1", null),
+                    Restaurante(2, "Restaurante 2",prods,2,"res2","Avenida Constitución 2", null),
+                    Restaurante(3, "Restaurante 3",prods,3,"res3","Avenida Constitución 3", null),
+                    Restaurante(4, "Restaurante 4",prods,4,"res4","Avenida Constitución 4", null),
+                    Restaurante(5, "Restaurante 5",prods,3,"res5","Avenida Constitución 5", null),
+                    Restaurante(6, "Restaurante 6",prods,5,"res6","Avenida Constitución 6", null),
+                    Restaurante(7, "Restaurante 7",prods,3,"res7","Avenida Constitución 7", null),
+                )
+                restdao.insertAll(restaurantes)
+                prodDao.insertAll(prods)
+                inserted = true
+            }
         }
     }
 
